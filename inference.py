d20b101108a51075f219447591f4ddd692343f5a
twin <twin@twin.com>
2023-06-23 14:59:53 -0600

    adjust max tokens

--- inference.py
+++ inference.py
@@ -11,7 +11,7 @@ class Predictor:
         self.tokenizer = AutoTokenizer.from_pretrained(model_dir, use_fast=True)
         
         print("Loading model...")
-        self.model = AutoGPTQForCausalLM.from_quantized(f"{model_dir}/", model_basename=model_basename, device="cuda:0", use_safetensors=True, use_triton=True)
+        self.model = AutoGPTQForCausalLM.from_quantized(f"{model_dir}/", max_new_tokens=500, model_basename=model_basename, device="cuda:0", use_safetensors=True, use_triton=True)
         
         self.pipeline = TextGenerationPipeline(model=self.model, tokenizer=self.tokenizer)
         
