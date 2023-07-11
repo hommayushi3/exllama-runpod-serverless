e44afc8a66de5738c88e2b22b5c4bec5b690ff83
Yushi Homma <yushihomma@Yushis-MacBook-Air.local>
2023-07-11 12:40:35 -0700

    Fixed streaming by changing runpod Python SDK version to one that supports streaming and updated example script

--- predict_stream.py
+++ predict_stream.py
@@ -2,6 +2,8 @@ import os
 import requests
 from time import sleep
 import logging
+import json
+import sys
 
 endpoint_id = os.environ["RUNPOD_ENDPOINT_ID"]
 URI = f"https://api.runpod.ai/v2/{endpoint_id}/run"
@@ -10,13 +12,13 @@ URI = f"https://api.runpod.ai/v2/{endpoint_id}/run"
 def run(prompt):
     request = {
         'prompt': prompt,
-        'max_new_tokens': 500,
+        'max_new_tokens': 1800,
         'temperature': 0.3,
         'top_k': 50,
         'top_p': 0.7,
         'repetition_penalty': 1.2,
         'batch_size': 8,
-        'stop': ['</s>']
+        'stream': True
     }
 
     response = requests.post(URI, json=dict(input=request), headers = {
@@ -26,26 +28,35 @@ def run(prompt):
     if response.status_code == 200:
         data = response.json()
         task_id = data.get('id')
-        return stream_output(task_id)
+        stream_output(task_id)
 
 
 def stream_output(task_id):
     try:
-        url = f"https://api.runpod.ai/v2/{endpoint_id}/status/{task_id}"
+        url = f"https://api.runpod.ai/v2/{endpoint_id}/stream/{task_id}"
         headers = {
             "Authorization": f"Bearer {os.environ['RUNPOD_AI_API_KEY']}"
         }
+
+        previous_output = ''
 
         while True:
             response = requests.get(url, headers=headers)
             if response.status_code == 200:
                 data = response.json()
+                if len(data['stream']) > 0:
+                    new_output = data['stream'][0]['output']
+                    sys.stdout.write(new_output[len(previous_output):])
+                    sys.stdout.flush()
+                    previous_output = new_output
+                    
                 if data.get('status') == 'COMPLETED':
-                    return data['output']
+                    break
+                    
             elif response.status_code >= 400:
                 logging.error(response.json())
-            # Sleep for 0.5 seconds between each request
-            sleep(0.5)
+            # Sleep for 0.05 seconds between each request
+            sleep(0.05)
     except Exception as e:
         print(e)
         cancel_task(task_id)
@@ -72,4 +83,4 @@ if __name__ == '__main__':
 -fh:father had MI recently,mother has thyroid dz
 -sh:non-smoker,mariguana 5-6 months ago,3 beers on the weekend, basketball at school
 -sh:no std,no other significant medical conditions."""
-    print(run(prompt))
+    run(prompt)
