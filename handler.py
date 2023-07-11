15401548767e5b43c3dcafeb79d2c1bbcab2447e
Yushi Homma <yushihomma@Yushis-MacBook-Air.local>
2023-07-11 09:53:45 -0700

    Changed return to yield in handler.py

--- handler.py
+++ handler.py
@@ -89,6 +89,6 @@ def inference(event) -> Union[str, Generator[str, None, None]]:
             yield res
     else:
         output_text = generator.generate_simple(prompt, max_new_tokens = max_new_tokens)
-        return output_text[len(prompt):]
+        yield output_text[len(prompt):]
 
 runpod.serverless.start({"handler": inference})
